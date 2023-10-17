5975d3424bdd390f59c68dcddf048973577d287b
Azbad <bazeezd4917@gmail.com>
2023-10-17 06:43:00 -0700

    commit for 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unstid.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -14,6 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
