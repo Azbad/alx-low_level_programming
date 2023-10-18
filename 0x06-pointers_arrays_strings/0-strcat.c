e8d056bb266cbe3c3be54c4460cce8b8e46205ea
Azbad <bazeezd4917@gmail.com>
2023-10-18 14:49:16 -0700

    commit for 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,24 +10,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int k, r;
+	int k, r;
 
-    k = 0;
-    r = 0;
+	k = 0;
+	r = 0;
 
-    while (dest[k] != '\0')
-    {
-        k++;
-    }
+	while (dest[k] != '\0')
+	{
+		k++;
+	}
 
-    while (src[r] != '\0')
-    {
-        dest[k] = src[r];
-        k++;
-        r++;
-    }
+	while (src[r] != '\0')
+	{
+		dest[k] = src[r];
+		k++;
+		r++;
+	}
 
-    dest[k] = '\0';
+	dest[k] = '\0';
 
-    return dest;
+	return dest;
 }
