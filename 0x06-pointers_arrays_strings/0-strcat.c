e5881bef172215f28adffb64fb56683092078ae3
Azbad <bazeezd4917@gmail.com>
2023-10-18 14:40:25 -0700

    commit for 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * strcat - function that concentrates two strings.
+ * _strcat - main function of the prototype
  *
- * @src: Concentration parameter
- * 
- * @dest: Concentration parameter
+ * @src: Concatenation parameter
+ * @dest: Concatenation parameter
  *
  * Return: dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-int k, r;
+    int k, r;
 
-k=0;
-r-0;
+    k = 0;
+    r = 0;
 
-while (dest[k] != '\0')
-{
-	k++;
-}
+    while (dest[k] != '\0')
+    {
+        k++;
+    }
 
-while (src[r] != '\0')
-{
-	dest[k] = src[r];
-	k++;
-	r++;
-}
-dest[k] = '\0';
-return (dest);
+    while (src[r] != '\0')
+    {
+        dest[k] = src[r];
+        k++;
+        r++;
+    }
+
+    dest[k] = '\0';
+
+    return dest;
 }
