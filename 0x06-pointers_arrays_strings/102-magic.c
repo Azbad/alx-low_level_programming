6bc747f072a06eb52428ff8c76d4dfc81ff1f095
Azbad <bazeezd4917@gmail.com>
2023-10-19 14:52:14 -0700

    commit for 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,16 +8,11 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
-	* write your line of code here...
-	* Remember:
-	* - you are not allowed to use a
-	* - you are not allowed to modify p
-	* - only one statement
-	* - you are not allowed to code anything else than this line of code
-	*/
-	p[5] = 98;
-	/* ...so that this prints 98\n */
+
+	/* Change the value at the memory location a[2] points to using pointer arithmetic */
+	*(p + 5) = 98;
+
+	/* Print the updated value of a[2] */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
