0cf126f2bfbbbd8828f44c1f7a35f84ae89d67e3
Azbad <bazeezd4917@gmail.com>
2023-10-19 15:20:04 -0700

    commit for 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+    int n;
+    int a[5];
+    int *p;
 
-	a[2] = 1024;
-	p = &n;
+    a[2] = 1024;
+    p = &n;
 
-	/* Change the value at the memory location a[2] points to using pointer arithmetic */
-	*(p + 5) = 98;
+    /* Your line of code here */
+    *(p + 5) = 98;
 
-	/* Print the updated value of a[2] */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+    /* ...so that this prints "a[2] = 98" followed by a new line */
+    printf("a[2] = %d\n", a[2]);
+    return (0);
 }
