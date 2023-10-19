1a6950ef48ae9bb11644abf2c9e7d5d11cb67546
Azbad <bazeezd4917@gmail.com>
2023-10-19 15:15:24 -0700

    commit for 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Print a buffer with specific formatting.
+ * print_buffer - main function
  *
  * @b: The buffer.
- * @size: The size of the buffer.
  *
- * Return: Always 0.
+ * @size: size of buffer
+ *
+ * Return: 0
  */
+
 void print_buffer(char *b, int size)
 {
 	int x;
@@ -19,40 +21,35 @@ void print_buffer(char *b, int size)
 
 	if (size <= 0)
 	{
-		printf("\n");
-		return;
+	printf("\n");
+	return;
 	}
-
 	while (x < size)
 	{
-		y = size - x < 10 ? size - x : 10;
-		printf("%08x: ", x);
+	y = size - x < 10 ? size - x : 10;
+	printf("%08x: ", x);
+	for (z = 0; z < 10; z++)
+	{
+	if (z < y)
+	printf("%02x", *(b + x + z));
+	else
+	printf("  ");
+	if (z % 2)
+	{
+	printf(" ");
+	}
+	}
+	for (z = 0; z < y; z++)
+	{
+	int c = *(b + x + z);
 
-		for (z = 0; z < 10; z++)
-		{
-			if (z < y)
-				printf("%02x", *(b + x + z));
-			else
-				printf("  ");
-
-			if (z % 2)
-			{
-				printf(" ");
-			}
-		}
-
-		for (z = 0; z < y; z++)
-		{
-			int c = *(b + x + z);
-
-			if (c < 32 || c > 126)
-			{
-				c = '.';
-			}
-			printf("%c", c);
-		}
-
-		printf("\n");
-		x += 10;
+	if (c < 32 || c > 132)
+	{
+	c = '.';
+	}
+	printf("%c", c);
+	}
+	printf("\n");
+	x += 10;
 	}
 }
