b056132efe8acc3fb675581b1427836e035c8b01
Azbad <bazeezd4917@gmail.com>
2023-10-19 13:50:25 -0700

    commit for 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * reverse_array - The main function
+ * reverse_array - Reverses an array of integers.
+ * @a: Array to be reversed.
+ * @n: Number of elements in the array.
  *
- * @a: Function parameter
- * @n: Function parameter
- *
- * Return: Always 0
+ * Return: Always void (no return).
  */
 void reverse_array(int *a, int n)
 {
-    int k, r;
+	int k, r;
 
-    for (k = 0; k < n--; k++)
-    {
-        r = a[k];
-        a[k] = a[n];
-        a[n] = r;
-    }
+	for (k = 0; k < n--; k++)
+	{
+		r = a[k];
+		a[k] = a[n];
+		a[n] = r;
+	}
 }
