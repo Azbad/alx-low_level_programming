08544dd7b35ee852ec8b39abcc66baf41939334e
Azbad <bazeezd4917@gmail.com>
2023-10-24 05:41:16 -0700

    commit for 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * print alphabet - Make the alphabet
  * Return: void
@@ -9,6 +8,7 @@
 void print_alphabet(void)
 {
 	char c;
+	
 	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		_putchar(c);
