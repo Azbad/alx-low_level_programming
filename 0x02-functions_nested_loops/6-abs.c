999f099dc149a8021f74f08ffec047559100b4f7
Azbad <bazeezd4917@gmail.com>
2023-10-28 14:02:52 -0700

    commit for static library libmy.a containing all functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of a number.
+ * @c: The number to compute the absolute value for.
+ *
+ * Return: The absolute value of the number.
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+		abs_val = c * -1;
+
+		return (abs_val);
+	}
+	return (c);
+}
