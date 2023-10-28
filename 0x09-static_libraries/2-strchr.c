3a56f74ca949bc9922e93305df6e6e8cfacdf05d
Azbad <bazeezd4917@gmail.com>
2023-10-28 14:34:57 -0700

    commit for create_static_lib.sh that creates a static library called liball.a

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdef.h>
+
 /**
  * _strchr - locates a character in a string
  *
@@ -9,13 +11,13 @@
  */
 char *_strchr(char *s, char c)
 {
-    int i;
+	int i;
 
-    for (i = 0; s[i] >= '\0'; i++)
-    {
-        if (s[i] == c)
-            return (&s[i]);
-    }
-    return (0);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return (NULL);
 }
-
