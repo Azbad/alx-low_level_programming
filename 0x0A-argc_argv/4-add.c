4696cab51d80098e2d7449272c0e76dd205def3f
Azbad <bazeezd4917@gmail.com>
2023-10-28 21:24:11 -0700

    commit for 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 
 /**
- * main - program adds positive number
+ * main - program adds positive numbers
  * @argc: number of command line arguments
  * @argv: array of command line arguments
  *
@@ -11,20 +11,20 @@
  */
 int main(int argc, char *argv[])
 {
-        int i, j, add = 0;
+	int i, j, add = 0;
 
-        for (i = 1; i < argc; i++)
-        {
-                for (j = 0; argv[i][j] != '\0'; j++)
-                {
-                        if (!isdigit(argv[i][j]))
-                        {
-                                printf("error\n");
-                                return (1);
-                        }
-                }
-                add += atoi(argv[i]);
-        }
-        printf("%d\n", add);
-        return (0);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
