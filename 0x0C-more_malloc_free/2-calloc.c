c1dc436294739761c9c9c5016406e488b147d47c
Azbad <bazeezd4917@gmail.com>
2023-11-02 19:40:59 -0700

    commit for 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * _calloc - function that allocates memory for an array, using malloc
@@ -16,10 +16,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	P = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0
+		p[i] = 0;
 	return (p);
 }
