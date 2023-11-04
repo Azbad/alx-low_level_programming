13168d37a4b1a731e9a72d18a912f7711e33484a
Azbad <bazeezd4917@gmail.com>
2023-11-03 18:37:19 -0700

    commit for 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,33 +11,33 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    char *nptr;
-    unsigned int i;
+	char *nptr;
+	unsigned int i;
 
-    if (new_size == old_size)
-        return ptr;
+	if (new_size == old_size)
+		return (ptr);
 
-    if (ptr == NULL)
-    {
-        nptr = malloc(new_size);
-    }
-    else if (new_size == 0)
-    {
-        free(ptr);
-        return NULL;
-    }
-    else
-    {
-        nptr = malloc(new_size);
-        if (nptr == NULL)
-            return NULL;
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
 
-        for (i = 0; i < old_size && i < new_size; i++)
-        {
-            nptr[i] = ((char *)ptr)[i];
-        }
+		for (i = 0; i < old_size && i < new_size; i++)
+		{
+			nptr[i] = ((char *)ptr)[i];
+		}
 
-        free(ptr);
-    }
-    return nptr;
+		free(ptr);
+	}
+	return (nptr);
 }
