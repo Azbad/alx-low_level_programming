e3d128430a1df708f0d35cfabed06ad61f88283b
Azbad <bazeezd4917@gmail.com>
2023-11-06 15:03:37 -0800

    commit for 0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+/**
+ * File name: 0-object_like_macro.h
+ *
+ * Description: Header file defines macro named SIZE
+ *
+ * Return: void
+ */
+#define SIZE 1024
+#endif
