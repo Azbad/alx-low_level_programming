0736a4a4784a2d2e1cb669dd606a5dec747c1286
Azbad <bazeezd4917@gmail.com>
2023-11-06 15:10:55 -0800

    commit for 1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef _MACRO_H
+#define _MACRO_H
+
+/**
+ * File name: 1-pi.h
+ *
+ * Description: Header file that defines a macro named PI
+ *
+ * Return: void
+ */
+#define PI 3.14159265359
+
+#endif
