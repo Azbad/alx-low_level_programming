d324b1b11e175a34982481009ff3b86c91137772
Azbad <bazeezd4917@gmail.com>
2023-11-06 15:49:42 -0800

    commit for 3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef _MACRO_H
+#define _MACRO_H
+
+/**
+ * File name: 3-function_like_macro.h
+ *
+ * @x: input number
+ *
+ * Description: header file that computes the absolute value of number
+ *
+ * Return: void
+ */
+#define ABS(x) (x < 0 ? (-1) * x : (x))
+
+#endif
