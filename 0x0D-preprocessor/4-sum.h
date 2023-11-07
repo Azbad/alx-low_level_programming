fb5bce5b4bdec887a8d44016774ac48920683ff1
Azbad <bazeezd4917@gmail.com>
2023-11-06 16:06:45 -0800

    commit for 4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef _MACRO_H
+#define _MACRO_H
+
+/**
+ * File name: 4-sum.h
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Description: header file computes sum of numbers x and y
+ *
+ */
+#define SUM(x, y) ((x) + (y))
+#endif
