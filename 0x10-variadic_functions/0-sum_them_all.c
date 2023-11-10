308b22dd930ddbd15af8e01734762476da82a226
Azbad <bazeezd4917@gmail.com>
2023-11-09 18:29:50 -0800

    commit for 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
-#include "VARIADIC_FUNCTIONS.H"
-#include <stdio.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - function returns sum of all parameters
@@ -18,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-	for (i = 0, i < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
 	va_end(ap);
