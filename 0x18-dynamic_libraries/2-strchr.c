c15e1d4a8b917bb5a2f829503714bf7aabd6d719
Azbad <atbazeezd4917@gmail.com>
2024-01-19 19:59:38 -0800

    create a dynamic library

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: The string to search
+ * @c: The character to locate
+ *
+ * Return: Pointer to the first occurrence of the character, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return (NULL);
+}
